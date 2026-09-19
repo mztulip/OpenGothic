12cd36900b5dba712bc21aaf117315004eb15907
mz <mateusz.zapart@columbusenergy.pl>
2026-09-19 23:34:13 +0200

    creating saves dir if this is not existing

--- common/commandline.cpp
+++ common/commandline.cpp
@@ -73,8 +73,12 @@ CommandLine::CommandLine(int argc, const char** argv) {
         // Ujednolicenie separatorów na '/' i dodanie slasha na końcu
         for(auto& c : savesPath)
           if(c == u'\\') c = u'/';
+
         if(!savesPath.empty() && savesPath.back() != u'/')
           savesPath.push_back(u'/');
+
+        if(!FileUtil::mkpath(savesPath))
+          Log::e("unable to create save directory: \"", TextCodec::toUtf8(savesPath), "\"");
         }
       }
     else if(arg=="-devmode") {
